0be9fcc5bf224aab6cd79b75583b1655997e80fe
AradD7 <arad.delaramnikoo@torontomu.ca>
2025-10-04 19:32:08 -0400

    added screenshots

--- src/main.py
+++ src/main.py
@@ -3,14 +3,15 @@ from point import Point
 from window import Window
 
 
-window_width = 3072 
+window_width = 3072
 window_height = 1620
-full_screen = True 
+full_screen = True
 
 
 def main():
     win = Window(window_width, window_height, full_screen)
-    the_maze = Maze(Point(50, 50), 22, 37, 75, 75, win)
+    the_maze = Maze(Point(50, 50), 28, 45, 60, 60, win)
+
     the_maze.solve()
 
 
